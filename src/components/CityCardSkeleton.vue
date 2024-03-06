e9e842e13b78359530fe79c50c7c7bd5c93ea9b5
wassim <63935826+wassimelboussi@users.noreply.github.com>
2024-03-07 01:05:51 +0400

    update keys

---
+++ src/components/CityCardSkeleton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex py-6 px-3 bg-weather-secondary rounded-md shadmow-md">
+    <div class="flex flex-col flex-1 gap-2">
+      <AnimatedPlaceHolder class="max-w-[50%]" />
+      <AnimatedPlaceHolder class="max-w-[40%]" />
+    </div>
+
+    <div class="flex flex-col items-end flex-1 gap-2">
+      <AnimatedPlaceHolder class="max-w-[50px]" />
+      <AnimatedPlaceHolder class="max-w-[75px] w-full" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AnimatedPlaceHolder from './AnimatedPlaceHolder.vue';
+
+
+</script>
